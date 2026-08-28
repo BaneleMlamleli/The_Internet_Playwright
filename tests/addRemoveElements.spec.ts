e2fed462cc35cca594edd42432298a078ec46973
BaneleMlamleli <mlamlelibanele@yahoo.com>
2026-08-28 02:10:36 +0200

    Add test for verifying button addition and removal

---
+++ tests/addRemoveElements.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+
+test("Verify button is added and removed", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  await page.getByText("Add/Remove Elements", { exact: true }).click();
+  await page.getByRole("button", { name: "Add Element" }).click();
+  await expect(page.getByRole("button", { name: "Delete" })).toBeVisible();
+  // TODO: Add assertion to see if button is added and visible
+  await page.getByRole("button", { name: "Delete" }).click();
+  await expect(page.getByRole("button", { name: "Delete" })).toBeVisible({
+    visible: false,
+  });
+});
