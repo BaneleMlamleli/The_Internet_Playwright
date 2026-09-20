5c308fbe5a368780ba484ef449586b27bba53921
BaneleMlamleli <mlamlelibanele@yahoo.com>
2026-09-20 23:55:41 +0200

    remove commented-out launchOptions for clarity in playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,6 +39,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+
+    // This may not work in other operating systems
+    // launchOptions: { args: ["--start-maximized"] },
   },
 
   /* Configure projects for major browsers */
